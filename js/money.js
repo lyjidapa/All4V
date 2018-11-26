7d7cafd399f484fd25cb5d7a1fab009b755501c4
pacawatk <pacawat.kan@gmail.com>
2018-11-26 14:36:51 +0700

    fix donate button on home page donate 1000 baht

--- js/money.js
+++ js/money.js
@@ -1,15 +1,17 @@
 var current1 = document.getElementById('current1');
 var percent1 = document.getElementById('percent1');
 var bar1 = document.getElementById('bar1');
-
+var donate1 = document.getElementById('donate1');
+var current;
+var goal;
 var ref = firebase.database().ref('patient1');
 
 ref.on("value", function(snapshot) {
   var json = snapshot.val();
    console.log(json.current);
    console.log(json.goal);
-   var current = json.current;
-   var goal = json.goal;
+   current = json.current;
+   goal = json.goal;
    setCampaign(goal,current);
 }, function (error) {
    console.log("Error: " + error.code);
@@ -18,6 +20,17 @@ ref.on("value", function(snapshot) {
 function setCampaign(goal,current){
   current1.innerText = current.toLocaleString()+' Baht';
   var percent = (goal-current)*100/goal;
-  percent1.innerText = percent+'%';
+
+  percent1.innerText = percent.toFixed(2)+'%';
   bar1.style.width = percent+'%';
 }
+
+donate1.addEventListener("click", function () {
+
+  var money = 1000;
+  current = current-money;
+  alert(current);
+ref.update({
+  "current": current
+});
+});
